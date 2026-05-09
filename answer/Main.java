89a69520e05c00d37ca3d7fb2f546d3a23a93f3c
Ganzhe2028 <gztxwd200828@qq.com>
2026-05-09 19:29:30 +0800

    Remove DequeQueue and DequeStack classes; update Main to use ArrayDeque for queue and stack operations

--- answer/Main.java
+++ answer/Main.java
@@ -5,32 +5,32 @@ import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayStack stack = new ArrayStack();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        System.out.println(stack.pop()); // 3
-
-        ArrayQueue queue = new ArrayQueue(3);
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        System.out.println(queue.dequeue()); // 1
-
-        // ---
 
         System.out.println("=== Deque as Queue ===");
-        Deque<Integer> dequeQueue = new ArrayDeque<>();
-        dequeQueue.offerLast(1);
-        dequeQueue.offerLast(2);
-        dequeQueue.offerLast(3);
-        System.out.println(dequeQueue.pollFirst()); // 1
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offerLast(1);
+        queue.offerLast(2);
+        queue.offerLast(3);
+        System.out.println(queue.pollFirst()); // == .peak()。注意.peak()是有返回值的，返回「队首指针」所指向的 element
 
         System.out.println("=== Deque as Stack ===");
-        Deque<Integer> dequeStack = new ArrayDeque<>();
-        dequeStack.push(10);
-        dequeStack.push(20);
-        dequeStack.push(30);
-        System.out.println(dequeStack.pop()); // 30
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack.pop()); // == .pop() 注意pop是有返回值的，返回被pop的element
+
+        System.out.println("=== Double-Ended Queue ===");
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.addLast(1);      // [1]
+        dq.addLast(2);   
+        // 此时为：[1, 2]
+
+        // 此时为：[0, 1, 2]    ← 在队首新增｜array 实现的 queue 做不到
+        dq.addFirst(0);     
+        System.out.println(dq.pollFirst());
+
+        System.out.println(dq.pollLast());      // 返回 2       ← 在队尾peek｜数组 queue 也做不到
+
     }
 }
